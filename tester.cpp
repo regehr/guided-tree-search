c347cf8118b69a464d83c9c199c9f5eefb352152
John Regehr <regehr@cs.utah.edu>
2022-01-23 14:34:58 -0700

    more testing work

--- tester.cpp
+++ tester.cpp
@@ -4,38 +4,84 @@
 
 #include "generator.h"
 
-static const int TreeDepth = 56;
-static uniform::Generator g;
+/*
+ * test tree generators
+ *
+ * when called, each of these functions uses the supplied generator to
+ * walk a single path through there decision tree that they
+ * implement. the return value is the number of the leaf that was
+ * reached by that traversal.
+ *
+ * the painful part of this interface is that leaf numbers should be
+ * fairly dense, ideally perfectly dense with range 0 .. NUM_LEAVES-1.
+ * so you have to map leaves to leaf numbers yourself-- an automated
+ * numbering scheme does not seem possible without fully enumerating
+ * the leaves (which won't be possible for many trees of interest)
+ */
 
-static unsigned long test1() {
-  for (int i = 0; i < TreeDepth; ++i) {
-    if (g.flip())
-      return i;
-  }
-  return TreeDepth;
+/*
+ * maximally unbalanced n-ary tree
+ */
+static unsigned long test1_helper(uniform::Generator &G, int Depth, int Number,
+                                  int BranchFactor) {
+  if (Depth == 0)
+    return Number;
+  auto Choice = G.choose(BranchFactor);
+  if (Choice != (BranchFactor - 1))
+    return Number + Choice;
+  return test1_helper(G, Depth - 1, Number + BranchFactor - 1, BranchFactor);
 }
 
-static unsigned long test2_helper(int Depth, int Number) {
-  const int Factor = 2;
+static unsigned long test1(uniform::Generator &G) {
+  const int TreeDepth = 5;
+  const int BranchFactor = 10;
+
+  return test1_helper(G, TreeDepth, 0, BranchFactor);
+}
+
+/*
+ * full tree
+ */
+static unsigned long test2_helper(uniform::Generator &G, int Depth, int Number,
+                                  int BranchFactor) {
   if (Depth == 0) {
     return Number;
   } else {
-    return test2_helper(Depth - 1, 2 * Number + f.choose(2));
+    assert(Depth > 0);
+    return test2_helper(G, Depth - 1,
+                        (BranchFactor * Number) + G.choose(BranchFactor),
+                        BranchFactor);
   }
 }
 
-static unsigned long test2() {
-  return test2_helper(TreeDepth, 1);
+static unsigned long test2(uniform::Generator &G) {
+  const int TreeDepth = 6;
+  const int BranchFactor = 2;
+
+  return test2_helper(G, TreeDepth, 0, BranchFactor);
 }
+
+/*
+ * TODO heavily skewed tree
+ */
+
+/*
+ * TODO somewhat unstructured tree
+ */
 
 int main() {
   const int REPS = 100 * 1000;
   std::vector<int> Results;
+  uniform::Generator G;
+
+  std::vector<unsigned long (*)(uniform::Generator &)> TreeGenerators = {test1,
+                                                                         test2};
 
   for (int rep = 0; rep < REPS; ++rep) {
-    if (!g.start())
+    if (!G.start())
       break;
-    auto Res = test2();
+    auto Res = test1(G);
+    std::cout << "Res = " << Res << "\n";
     if (Res >= Results.size())
       Results.resize(Res + 1);
     ++Results.at(Res);
