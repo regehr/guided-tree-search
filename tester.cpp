85758d85e896731e7b3a1ba9f4b31815d1acfafd
John Regehr <regehr@cs.utah.edu>
2022-01-22 21:38:30 -0700

    messing with the tester mostly

--- tester.cpp
+++ tester.cpp
@@ -4,11 +4,10 @@
 
 #include "generator.h"
 
-static const int TreeDepth = 10;
-static std::vector<int> results(TreeDepth + 1);
+static const int TreeDepth = 55;
 static uniform::Generator g;
 
-static int test() {
+static unsigned long test1() {
   for (int i = 0; i < TreeDepth; ++i) {
     if (g.flip())
       return i;
@@ -16,20 +15,34 @@ static int test() {
   return TreeDepth;
 }
 
+static unsigned long test2_helper(int Depth) {
+  if (Depth == 0)
+    ;
+  else
+    return test2_helper(Depth - 1);
+}
+
+static unsigned long test2() {
+  return test2_helper(TreeDepth);
+}
+
 int main() {
-  const int REPS = 100000;
+  const int REPS = 100 * 1000;
+  std::vector<int> Results;
 
   for (int rep = 0; rep < REPS; ++rep) {
     if (!g.start())
       break;
-    auto res = test();
-    results[res]++;
+    auto Res = test1();
+    if (Res >= Results.size())
+      Results.resize(Res + 1);
+    ++Results.at(Res);
   }
 
   int total = 0;
-  for (int i = 0; i <= TreeDepth; i++) {
-    std::cout << i << " : " << results[i] << "\n";
-    total += results[i];
+  for (unsigned long i = 0; i < Results.size(); ++i) {
+    std::cout << i << " : " << Results.at(i) << "\n";
+    total += Results.at(i);
   }
   std::cout << "total = " << total << "\n";
   assert(total == REPS);
